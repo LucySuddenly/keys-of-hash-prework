038de5ada2f2d4667c1a70b426d3a373f4028e54
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-19 18:35:46 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,9 @@ class Hash
   def keys_of(*arguments)
     array = []
     arguments.each do |animal, home|
-      if 
+      if home == arguments
+        array << animal
+      end 
     end
     array
   end
